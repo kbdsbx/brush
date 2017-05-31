8bb03fed1f0a810adfc9538e6c26aa03572153c0
狂奔的扫把星 <z@zhaoyi.me>
2017-05-31 17:57:07 +0800

    added project page and resume.

--- demo/index/script.js
+++ demo/index/script.js
@@ -15,12 +15,14 @@
     $( window ).on( 'load', _resize );
 
     if ( window.location.hash ) {
+        var _hash = window.location.hash.substr( 1 );
         setTimeout( function() {
             $( '.main-section' ).addClass( 'hide' );
             $( window.location.hash ).addClass( 'show' );
             $( '.main-section .next-box' ).removeClass( 'show' ).addClass( 'never' );
             $( '.nav' ).addClass( 'show' );
             $( '.nav li a[href="' + window.location.hash + '"]').parent().addClass( 'active' );
+            $( '.nav' ).addClass( _hash + '-bg' );
         }, 400 );
     }
 
@@ -48,6 +50,8 @@
         $( '.nav li' ).removeClass( 'active' );
         $( this ).parent( 'li' ).addClass( 'active' );
         var _new = $( '.nav li.active a' ).attr( 'href' );
+        var _hash = _new.substr( 1 );
+        $( '.nav' ).removeClass( 'product-bg, project-bg' ).addClass( _hash + '-bg' );
 
         if ( !_old ) {
             $( '.main-section' ).addClass( 'hide' );
